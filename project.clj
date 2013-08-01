1fe6bb1fd3896487edbe7d933842391894fdf546
anatoly <tolitius@gmail.com>
2013-08-01 12:50:39 -0400

    >! 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject monr "0.1.3-SNAPSHOT"
+(defproject monr "0.1.5-SNAPSHOT"
   :description "\"anything\" rate monitor"
   :url "https://github.com/tolitius/monr"
   :license {:name "Eclipse Public License"
