ac7fdfdcfc1b90b8edb606a058468c0145d17479
Anatoly <tolitius@gmail.com>
2015-10-29 22:37:46 -0400

    ;; TODO for a queue publisher sample

--- src/monr/report.clj
+++ src/monr/report.clj
@@ -13,3 +13,9 @@
 (format "\n| %17s  | %,13d ops/sec | %,26d |" id (long rate) current)
 "\n\\-------------------------------------------------------------------------/")))
 
+;; TODO sample for a queue publisher i.e.
+
+#_(defn publish [{:keys [id rate current] :as rate}]
+  ;; ...
+  (send queue rate))
+
